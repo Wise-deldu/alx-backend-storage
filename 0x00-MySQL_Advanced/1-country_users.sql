674ef040d8046d74bbab5eb8c1975cbfc29639ac
Wise-deldu <delduwise@gmail.com>
2024-08-23 02:22:31 +0000

    An SQL script that creates a table users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates a table with unique users.
+DROP TABLE IF EXITS users;
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAUlT 'US' CHECK (country IN ('US', 'CO', 'IN'))
+);
