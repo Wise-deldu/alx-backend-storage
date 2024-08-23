1eeb966f34f42ffc48da758fecb20f3a6274473a
Wise-deldu <delduwise@gmail.com>
2024-08-23 02:25:33 +0000

    Rectified a word error

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- Creates a table with unique users.
-DROP TABLE IF EXITS users;
+DROP TABLE IF EXISTS users;
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
