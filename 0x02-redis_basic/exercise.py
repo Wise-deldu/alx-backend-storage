7816dc61a0dac0dcd8dcf541a8bf5e9717de51f7
Wise-deldu <delduwise@gmail.com>
2024-11-04 15:33:55 +0000

    Pycodestyle validation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -80,7 +80,7 @@ class Cache:
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
         return data_key
-    
+
     def get(
             self,
             key: str,
@@ -90,12 +90,12 @@ class Cache:
         '''
         data = self._redis.get(key)
         return fn(data) if fn is not None else data
-    
+
     def get_str(self, key: str) -> str:
         '''Retrieves a string value from a Redis data storage.
         '''
         return self.get(key, lambda x: x.decode('utf-8'))
-    
+
     def get_int(self, key: str) -> int:
         '''Retrieves an integer value from a Redis data storage.
         '''
